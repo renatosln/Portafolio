7167988ae1508a8f926896fe2efe66385ee2dd2c
renatosln <renato123.l.n@gmail.com>
2022-10-31 02:39:09 -0500

    Primer avance

---
+++ assets/main/main.js
@@ -0,0 +1,6 @@
+const iconMenu = document.querySelector(".icon_menu");
+const menu = document.querySelector(".menu");
+
+iconMenu.addEventListener('click',function(){
+    menu.classList.toggle("menu_show");
+});
